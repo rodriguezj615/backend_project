b42f64edea3a46f533e2d5fb9b93d8c7e843168b
Juanchi <rodriguezj615@hotmail.com>
2021-05-27 12:18:53 -0300

    Ajuste de layout

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -6,14 +6,44 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{ asset("css/dashboard.css") }}">
         <title>@yield('title')</title>
     </head>
     <body>
-        {{-- @include('components.header') --}}
-        <div class="container">
-            {{-- @include('components.alert') --}}
-            @yield('content')
+      <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">MundosE</a>
+        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
+            data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+      </header>
+      <div class="container-fluid">
+        <div class="row">
+            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+              <div class="position-sticky pt-3">
+                  <ul class="nav flex-column">
+                      <li class="nav-item">
+                          <a class="nav-link active" aria-current="page" href="#">
+                              <span data-feather="home"></span>
+                              Dashboard
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="#">
+                              <span data-feather="file"></span>
+                              Orders
+                          </a>
+                      </li>
+                  </ul>
+              </div>
+            </nav>
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+              @yield("content")
+            </main>
         </div>
+      
+      </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     </body>
 </html>
--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,10 +1,8 @@
 @extends('base')
 @section('title') Inicio @endsection
 @section('content')
-    <main class="container-fluid">
-        <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-            <h1 class="display-4">ABM de Tareas</h1>
-            <p class="lead">Ejercicio práctico de un ABM completo de un modelo de tarea y categoría.</p>
-        </div>
-    </main>
+    <div
+        class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Dashboard</h1>
+    </div>
 @endsection
