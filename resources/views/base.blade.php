32a2b852ca799af8ced1596549a55b1a50f0f0e8
Juanchi <rodriguezj615@hotmail.com>
2021-08-12 17:13:19 -0300

    pin1.0
    1.0

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -7,6 +7,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
         <link rel="stylesheet" href="{{ asset("css/dashboard.css") }}">
+        
         <title>@yield('title')</title>
     </head>
     <body>
@@ -24,15 +25,9 @@
               <div class="position-sticky pt-3">
                   <ul class="nav flex-column">
                       <li class="nav-item">
-                          <a class="nav-link active" aria-current="page" href="{{ route ("dashboard") }}">
+                          <a class="nav-link active" aria-current="page" href="{{route("dashboard")}}">
                               <span data-feather="home"></span>
                               Dashboard
-                          </a>
-                      </li>
-                      <li class="nav-item">
-                          <a class="nav-link" href="#">
-                              <span data-feather="file"></span>
-                              Orders
                           </a>
                       </li>
                   </ul>
--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -3,6 +3,6 @@
 @section('content')
     <div
         class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Dashboard</h1>
+        <h1 class="h2">Test BDD</h1>
     </div>
 @endsection
