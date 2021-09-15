655a1dc01675dd5499f007462d7d5fc3a3cfdc4f
Juanchi <rodriguezj615@hotmail.com>
2021-09-15 20:26:23 -0300

    1
    a

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -5,4 +5,7 @@
         class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Test BDD</h1>
     </div>
+    <h4>
+        Pagina representativa solo para chequeo del formulario
+    </h4>
 @endsection
